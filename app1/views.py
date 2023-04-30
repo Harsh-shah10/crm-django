dd6a4c77deea82688c016599ba053f5afe29d6bc
Harsh-shah10 <180120107052@git.org.in>
2023-04-30 19:25:45 +0530

    added login modified homeview

--- app1/views.py
+++ app1/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# django login logout modules
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 
 def homepage(request):
-    
-    return render(request, 'homepage.html')
+    if request.method == 'POST':
+        Username = request.POST.get('Username')
+        Password = request.POST.get('Password')
+        # auth logic 
+        user = authenticate(request, username=Username, password=Password)
+        if user:
+            login(request, user)
+            messages.success(request, "User logged in successfully !")
+            return redirect('homepage')
+        else:
+            messages.success(request, "Invalid Username or Password !")
+            return redirect('homepage')
+    else:
+        return render(request, 'homepage.html')
     
 def user_login(request):
     pass
-    
 
 def user_logout(request):
-    pass
+    logout(request)
+    messages.success(request, "User LoggedOut!")
+    return render(request, 'homepage.html')
+    
+    
